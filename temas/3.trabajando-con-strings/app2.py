204966fa5bd39b61e7ea516cfc1aa517b48d4034
Carlos G. Mogollón A <152676014+dekardinuta@users.noreply.github.com>
2024-10-01 18:53:02 -0400

    feat: agregar caracteristicas de variables de cadena

---
+++ temas/3.trabajando-con-strings/app2.py
@@ -0,0 +1,58 @@
+#Creacion de cadenas de texto
+#print("Giraffe Academy")
+
+#Salto de linea
+#print("Giraffe\nAcademy")
+
+#Comilla doble
+#print("Giraffe\"Academy")
+
+#barra invertida
+#print("Giraffe\Academy")
+
+#Variable de cadena
+#phrase = "Giraffe Academy"
+#print(phrase)
+
+#Concatenacion de cadenas
+#phrase = "Giraffe Academy"
+#print(phrase + " is cool")
+
+#Mayusculas y minusculas
+#phrase = "Giraffe Academy"
+#print(phrase.lower())
+#print(phrase.upper())
+
+#Es mayuscula?
+#phrase = "Giraffe Academy"
+#print(phrase.isupper())
+
+#Combinacion de funciones
+#phrase = "Giraffe Academy"
+#print(phrase.upper().isupper())
+
+#Calcular la longitud de una cadena
+#phrase = "Giraffe Academy"
+#print(len(phrase))
+
+'''
+Caracteres individuales en una cadena
+phrase = "Giraffe Academy"
+          0123 
+print(phrase[0])
+'''
+
+#phrase = "Giraffe Academy"
+#print(phrase[3])
+
+#Funcion de indice
+#phrase = "Giraffe Academy"
+#print(phrase.index("G"))
+#print(phrase.index("a"))
+#print(phrase.index("Acad"))
+#print(phrase.index("z"))
+
+#Funcion de reemplazo
+phrase = "Giraffe Academy"
+print(phrase.replace("Giraffe", "Elephant"))
+
