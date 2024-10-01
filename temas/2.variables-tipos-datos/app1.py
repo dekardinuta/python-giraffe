6f35b413c787672777b61b6d4a0a4ae8fd4069da
Carlos G. Mogollón A <152676014+dekardinuta@users.noreply.github.com>
2024-10-01 15:28:31 -0400

    feat: Agregar funcionalidad de variables a app1.py

---
+++ temas/2.variables-tipos-datos/app1.py
@@ -0,0 +1,9 @@
+character_name = "Tom"
+character_age = 50
+is_male = False
+print("Había una vez un hombre llamado " + character_name + ", ")
+print("tenía " + str(character_age) + " años.")
+
+character_name = "Mike"
+print("Le gustaba mucho el nombre " + character_name + ", ")
+print(f"pero no le gustaba tener {character_age}.")
