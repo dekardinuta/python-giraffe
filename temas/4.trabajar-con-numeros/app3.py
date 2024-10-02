f5cd39349322c6172ad7d71bd9ca37ef9f2acfbe
Carlos G. Mogollón A <152676014+dekardinuta@users.noreply.github.com>
2024-10-01 23:54:30 -0400

    feat: caracteristicas de operar con numeros

---
+++ temas/4.trabajar-con-numeros/app3.py
@@ -0,0 +1,32 @@
+# print(2)
+# print(2.0987)
+# print(-2.0987)
+# print(3 + 4.6)
+# print(3 * 4 + 5)
+#print(3 * (4 + 5))
+#print(10 % 3)
+'''
+my_num = 5
+print(str(my_num) + " Es mi numero favorito" )
+'''
+'''
+my_num = -5
+print(abs(my_num))
+'''
+
+#print(pow(4, 2))
+#print(max(4,6))
+#print(min(4, 6))
+#print(round(3.2))
+#print(round(3.5))
+'''
+from math import *
+print(floor(3.7))
+print(ceil(3.7))
+print(sqrt(36))
+'''
+
+
+
+
+
