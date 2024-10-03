12fd6b87a325c8d6eb19bef1e5fe95fe7a9c6e53
Carlos G. Mogollón A <152676014+dekardinuta@users.noreply.github.com>
2024-10-03 01:25:17 -0400

    feat: procesar suma de numeros flotantes en app5.py

---
+++ temas/6.crear-una-calculadora-basica/app5.py
@@ -0,0 +1,14 @@
+num1 = input("Enter a number: ")
+num2 = input("Enter anther number: ")
+
+# Numeros como cadenas
+#result = num1 + num2
+#print(result) Imprimira una concatenacion de caracteres
+
+# Numeros como enteros
+#result = int(num1) + int(num2)
+#print(result) # Imprime solo si ambos numeros son enteros
+
+# Numeros como flotantes
+result = float(num1) + float(num2)
+print(result)
