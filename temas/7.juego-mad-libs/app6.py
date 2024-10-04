c2dc9dc6b333f05b8ecc687e60e90591512bdfc3
Carlos G. Mogollón A <152676014+dekardinuta@users.noreply.github.com>
2024-10-03 21:30:44 -0400

    feat: implementar juego de mad libs

---
+++ temas/7.juego-mad-libs/app6.py
@@ -0,0 +1,19 @@
+print("Las Rosas son rojas,")
+print("las Violetas son azules,")
+print("Me encanta la programación.")
+
+# Estuctura basica para nuestro mad libs
+
+print("Las Rosas son {color},")
+print("{plural_noun} son azules,")
+print("Me encanta {celebrity}.")
+
+# Variables de entrada
+
+color = input("Enter a color: ")
+plural_noun = input("Enter a plural noun: ")
+celebrity = input("Enter a celebrity: ")
+
+print(f"Las Rosas son {color},")
+print(f"{plural_noun} son azules,")
+print(f"Me encanta {celebrity}.")
