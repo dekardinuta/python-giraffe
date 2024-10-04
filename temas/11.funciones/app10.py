5b1c2c04c97e54d1e73ffa6aabead12da06785d8
Carlos G. Mogollón A <152676014+dekardinuta@users.noreply.github.com>
2024-10-04 18:07:08 -0400

    feat: establecer la definicion y uso de las funciones

---
+++ temas/11.funciones/app10.py
@@ -0,0 +1,22 @@
+
+#* funciones 
+#* def sayhi():
+    #* print("Hello User")
+
+#! para llamar a la funcion debo:
+#! sayhi()
+
+#? como funciona las funciones:
+#? print("Top")
+#? sayhi()
+#? print("Bottom")
+
+#* Agregar parametros
+def say_hi(name, age):
+    print("Hello " + name + ", you are " + str(age))
+
+say_hi("Mike", 35)
+say_hi("Steve", 70)
+
+
+
